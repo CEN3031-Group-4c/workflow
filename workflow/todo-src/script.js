e485740926a952b54079f475bc4fb471fe6bd214
Your Name <patricpoplawska@gmail.com>
2015-09-14 23:52:37 -0400

    Added Clear List Func

---
+++ workflow/todo-src/script.js
@@ -0,0 +1,41 @@
+// Code goes here
+
+var myApp = angular.module('app', []);
+
+myApp.controller('MainCtrl', function ($scope){
+  $scope.todos = ["Learn Angular", "Learn node"];
+  $scope.newItem = "";
+
+  $scope.addItem = function(){
+    console.log("in add");
+    if ($scope.newItem !== ""){
+      $scope.todos.push($scope.newItem);
+      $scope.newItem = "";
+    }
+  }
+
+  $scope.clearList = function(){
+    console.log("in clear all");
+    $scope.todos.length = 0;
+  }
+
+  $scope.deleteItem = function(item){
+    console.log("in delete");
+    var index = $scope.todos.indexOf(item);
+    $scope.todos.splice(index, 1);
+  }
+
+
+});
+
+/*************************
+ * Homework (not rly):
+ * - "enter" button functionality instead of clicking button
+ * - edit button functionality
+ * - button to mark item as "complete"
+ * - have a total number of items at the top
+ * - make it prettier
+ * - add a due date
+ * - add reminder (setInterval)
+ *
+ * *********************/
